f2adf86de3bf0e3cbe0438b537966e0b4f1231a6
Kove W. Ochre-Salter <kove.w.o.salter@gmail.com>
2018-10-07 16:15:37 +1000

    attempt to fix bug in rooms.php

--- global.php
+++ global.php
@@ -4,7 +4,7 @@ echo '<br>';
 var_dump($global_rooms_xml);
 echo '<br>';
 
-$global_rooms = $global_rooms_xml->children()->room;
+$global_rooms = $global_rooms_xml->children();
 echo '<br>';
 var_dump($global_rooms);
 echo '<br>';
