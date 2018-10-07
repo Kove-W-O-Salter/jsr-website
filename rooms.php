105f1c437a5a27e66106d197f92570bec93119dd
Kove W. Ochre-Salter <kove.w.o.salter@gmail.com>
2018-10-07 14:50:04 +1000

    try making room-binding code global to rooms.php

--- rooms.php
+++ rooms.php
@@ -1,19 +1,19 @@
 <?php require './global.php'; ?>
+<?php
+    $room_id = $_GET['room_id'];
+    $room = $rooms[$room_id - 1];
+    echo '<br> ROOM_ID = ' . $room_id . '<br>';
+    echo '<br> ROOM_DUMP = ';
+    var_dump($room);
+    echo '<br>';
+    echo '<br> ACTUAL_ROOM_ID = ' . $room->id . '<br>';
+    echo '<br> ACTUAL_ROOM_NAME = ' . $room->name . '<br>';
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
     	<link rel="stylesheet" href="./style.css">
-    	<?php
-    		$room_id = $_GET['room_id'];
-    		$room = $rooms[$room_id - 1];
-            echo '<br> ROOM_ID = ' . $room_id . '<br>';
-            echo '<br> ROOM_DUMP = ';
-            var_dump($room);
-            echo '<br>';
-            echo '<br> ACTUAL_ROOM_ID = ' . $room->id . '<br>';
-            echo '<br> ACTUAL_ROOM_NAME = ' . $room->name . '<br>';
-    	?>
     </head>
     <body>
         <?php require './header.php'; ?>
