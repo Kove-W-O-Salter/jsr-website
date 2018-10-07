28535e52dbba23265e92ae351b297c760ddf40c5
Kove W. Ochre-Salter <kove.w.o.salter@gmail.com>
2018-10-07 16:37:42 +1000

    attempt to fix bug in rooms.php

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
                 <div class="banner_navbar_dropdown_button"> ROOMS </div>
                 <div class="banner_navbar_dropdown_items">
                     <?php
-                    foreach ($rooms as $header_room) {
+                    foreach ($global_rooms as $header_room) {
                        echo '<a href=\'./rooms.php?room_id=' . $header_room->id . '\'>' . $header_room->name . '</a>';
                     }
                     ?>
