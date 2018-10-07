e46e0c8f34c6e40660d31d5a406eb40d465e5ed2
Kove W. Ochre-Salter <kove.w.o.salter@gmail.com>
2018-10-07 16:48:38 +1000

    start cleanup in rooms.php

--- rooms.php
+++ rooms.php
@@ -1,8 +1,8 @@
 
-<?php require './global.php'; ?>
 <?php
-    $rooms_room_id = $_GET['room_id'];
-    $rooms_room = $global_rooms[$rooms_room_id - 1] or die('No such room, with the id \'' . $rooms_room_id . '\', exists.');
+require './global.php';
+$rooms_room_id = $_GET['room_id'];
+$rooms_room = $global_rooms[$rooms_room_id - 1] or die('No such room, with the id \'' . $rooms_room_id . '\', exists.');
 ?>
 
 <!DOCTYPE html>
