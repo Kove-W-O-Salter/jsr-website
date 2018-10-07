c22888ed37e040509ae48c7f1114b3a4fdaa1daa
Kove W. Ochre-Salter <kove.w.o.salter@gmail.com>
2018-10-07 17:04:01 +1000

    add contact.php and contact.xml

---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php
+$contact_info = simplexml_load_file('./contact.xml');
+?>
+<?php require './global.php'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    	<link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <?php require './header.php'; ?>
+        <div class="container">
+        	<table>
+        		<tr>
+        			<th>Contact Method</th>
+        			<th>Contact Address/Phone</th>
+        		</tr>
+        		<tr>
+        			<td>Email</td>
+        			<td><?php echo $contact_info->email; ?></td>
+        		</tr>
+        		<tr>
+        			<td>Phone</td>
+        			<td><?php echo $contact_info->phone; ?></td>
+        		</tr>
+        	</table>
+        </div>
+    </body>
+</html>
