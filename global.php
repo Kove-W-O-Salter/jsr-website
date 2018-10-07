2b1a6970c45620e194ae0229d383018baac131a9
Kove W. Ochre-Salter <kove.w.o.salter@gmail.com>
2018-10-07 15:54:56 +1000

    attempt to fix rooms.php

--- global.php
+++ global.php
@@ -1,5 +1,10 @@
 <?php
-$global_rooms = simplexml_load_file('./rooms.xml')->room or die('Cannot parse rooms.xml');
+$global_rooms_xml = simplexml_load_file('./rooms.xml') or die('Cannot parse rooms.xml');
+echo '<br>';
+var_dump($global_rooms_xml);
+echo '<br>';
+
+$global_rooms = $global_rooms_xml->children();
 echo '<br>';
 var_dump($global_rooms);
 echo '<br>';
