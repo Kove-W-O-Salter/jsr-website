a970c4dacb01ecd06707e78a59bc3b351c36332d
Kove W. Ochre-Salter <kove.w.o.salter@gmail.com>
2018-10-07 16:42:41 +1000

    improve code in rooms.php

--- rooms.php
+++ rooms.php
@@ -2,7 +2,7 @@
 <?php require './global.php'; ?>
 <?php
     $rooms_room_id = $_GET['room_id'];
-    $rooms_room = $global_rooms[$rooms_room_id - 1] or die('No such room, with the id \'' . $rooms_room_id . '\', exists.');
+    $rooms_room = $global_rooms[$rooms_room_id] or die('No such room, with the id \'' . $rooms_room_id . '\', exists.');
 ?>
 
 <!DOCTYPE html>
