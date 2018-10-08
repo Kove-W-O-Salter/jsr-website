74536ba6f624900dca655f4c689b71f9dfbad698
Kove W. Ochre-Salter <kove.w.o.salter@gmail.com>
2018-10-08 11:57:58 +1000

    fix slideshow styles.sass

--- rooms.php
+++ rooms.php
@@ -29,10 +29,10 @@ $rooms_room = $global_rooms[$rooms_room_id - 1] or die('No such room, with the i
                 // }
                 ?>
 
-                <center>
-                    <button class="normal_button" onclick="plusDivs(-1)">&#10094;</button>
-                    <button class="normal_button" onclick="plusDivs(+1)">&#10095;</button>
-                </center>
+                <!-- <center> -->
+                    <!-- <button class="normal_button" onclick="plusDivs(-1)">&#10094;</button> -->
+                    <!-- <button class="normal_button" onclick="plusDivs(+1)">&#10095;</button> -->
+                <!-- </center> -->
             </div>
             <?php echo $rooms_room->description; ?>
             
