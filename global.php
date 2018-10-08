832b61aca50f17f029f32fcbba818792bcd75172
Kove W. Ochre-Salter <kove.w.o.salter@gmail.com>
2018-10-08 13:10:57 +1000

    fix slideshow

--- global.php
+++ global.php
@@ -4,6 +4,7 @@ $global_rooms = $global_rooms_xml->children();
 
 function slideshow($slideshow_images) {
 	echo '<div class=\'slideShow\'>';
+    echo '<script src=\'/js/slideshow.js\'/>';
 
     if ($slideshow_images->count() == 0) {
         echo '<center>No images available.</center>';
@@ -17,7 +18,6 @@ function slideshow($slideshow_images) {
         echo '<button class="normal_button" onclick="plusDivs(+1)">&#10095;</button></center>';
     }
 
-    echo '<script src=\'/js/slideshow.js\'/>';
     echo '</div>';
 }
 ?>
